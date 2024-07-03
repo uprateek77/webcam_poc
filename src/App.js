c613caa417f6178413384ad0750dececd9bd4fc6
uprateek77 <uprateek77@gmail.com>
2024-07-03 17:14:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactPlayer from "react-player";
+import Webcam from "react-webcam";
+import demoVedio from "./assets/demoVedio.mp4";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>WebCam POC</h1>
+      <div className="webcam-container">
+        <Webcam className="webcam" />
+        <ReactPlayer
+          url={demoVedio}
+          playing={true} 
+    loop={true} 
+    controls={false}
+          className="react-player"
+          width={"30rem"}
+          height={"30rem"}
+        />
+      </div>
     </div>
   );
 }
