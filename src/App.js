28ceb522fd32e063977c4f3db466dccc33594979
uprateek77 <uprateek77@gmail.com>
2024-07-03 17:51:02 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
+// import ReactPlayer from "react-player";
+// import Webcam from "react-webcam";
+// import demoVedio from "./assets/demoVedio.mp4";
+// import "./App.css";
+
+// function App() {
+//   const videoConstraints = {
+//     width: 4096,
+//     height: 2160,
+//     facingMode: "environment"
+//   };
+//   return (
+//       <div className="webcam-container">
+//         <Webcam className="webcam"  videoConstraints={videoConstraints}/>
+//         <ReactPlayer
+//           url={demoVedio}
+//           playing={true}
+//           loop={true}
+//           controls={false}
+//           className="react-player"
+//           width={"20rem"}
+//           height={"30rem"}
+         
+//         />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import ReactPlayer from "react-player";
 import Webcam from "react-webcam";
 import demoVedio from "./assets/demoVedio.mp4";
 import "./App.css";
 
 function App() {
+  const videoConstraints = {
+    width: 4096,
+    height: 2160,
+    facingMode: "environment"
+  };
   return (
-    <div className="App">
-      <h1>WebCam POC</h1>
-      <div className="webcam-container">
-        <Webcam className="webcam" />
-        <ReactPlayer
-          url={demoVedio}
-          playing={true} 
-    loop={true} 
-    controls={false}
-          className="react-player"
-          width={"30rem"}
-          height={"30rem"}
-        />
-      </div>
+    <div className="webcam-container">
+      <Webcam className="webcam" videoConstraints={videoConstraints} />
+      <ReactPlayer
+        url={demoVedio}
+        playing={true}
+        loop={true}
+        controls={false}
+        className="react-player"
+        width={"20rem"}
+        height={"30rem"}
+      />
     </div>
   );
 }
